b0e425bf76be34f6979739951f89df688bf48753
VavakinV <vladvavakin@gmail.com>
2025-07-10 16:36:09 +0300

    modified startup UI

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from train_all_models import train_all_models
 from fullmodel_test import run_tests
 
 if __name__ == "__main__":
-    train_all_models()
+    train_models = input("Train models?\n('No' -> Enter, 'Yes' -> <any key>+Enter): ")
+    if train_models:
+        train_all_models()
     run_tests()
