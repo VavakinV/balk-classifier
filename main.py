99c081987730f085b6106450f8ecfc6751dac9ad
VavakinV <vladvavakin@gmail.com>
2025-07-10 13:44:14 +0300

    UI fix

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from fullmodel_test import run_tests
 if __name__ == "__main__":
     choice = int(input("Which model to train?\n1 -> Only bbox\n2 -> Only classification\n3 -> Both\n0 -> None\nChoose: "))
     try:
-        if train_models:
-            train_models(choice)
+        train_models(choice)
+        run_tests()
     except:
         print("Invalid input")
-        exit
-    run_tests()
+    
