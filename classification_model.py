f643650a448799ecb62f2731d63f5d77395eaf53
VavakinV <vladvavakin@gmail.com>
2025-07-11 15:42:05 +0300

    added base model efficientnet b0 for classification

--- classification_model.py
+++ classification_model.py
@@ -1,30 +1,30 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import models
 
 class ProducerClassifier(nn.Module):
     def __init__(self, num_classes=5):
         super().__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(16, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-        self.classifier = nn.Sequential(
-            nn.Linear(64 * 40 * 40, 128),
-            nn.ReLU(),
+        self.base_model = models.efficientnet_b0(pretrained=True)
+
+        for param in self.base_model.parameters():
+            param.requires_grad = False
+
+        for param in self.base_model.features[5:].parameters():
+            param.requires_grad = True
+        for param in self.base_model.features[7:].parameters():
+            param.requires_grad = True
+
+        in_features = self.base_model.classifier[1].in_features
+        self.base_model.classifier = nn.Sequential(
             nn.Dropout(0.5),
-            nn.Linear(128, num_classes)
+            nn.Linear(in_features, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(512, num_classes)
         )
 
     def forward(self, x):
-        x = self.features(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        return x
+        return self.base_model(x)
